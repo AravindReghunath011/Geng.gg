74dd256cd456d050df31bd64a7a3cc1f204efe4e
Aravind <aravindreghunath99@gmail.com>
2024-08-22 09:09:57 +0530

    initial

---
+++ src/Components/Grid.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Grid = () => {
+  return (
+    <div className='flex w-full h-full z-10 m-auto absolute inset-0'>
+        <div className='w-full h-full border-l-0.5 border-x-gray-300 border-opacity-30 '></div>
+        <div className='w-full h-full border-l-0.5 border-x-gray-300 border-opacity-30'></div>
+        <div className='w-full h-full border-l-0.5 border-x-gray-300 border-opacity-30'></div>
+        <div className='w-full h-full border-l-0.5 border-x-gray-300 border-opacity-30'></div>
+        <div className='w-full h-full border-l-0.5 border-x-gray-300 border-opacity-30'></div>
+        <div className='w-full h-full border-l-0.5 border-x-gray-300 border-opacity-30'></div>
+        <div className='w-full h-full border-l-0.5 border-x-gray-300 border-opacity-30'></div>
+        <div className='w-full h-full border-l-0.5  border-x-gray-300 border-opacity-30'></div>
+    </div>
+  )
+}
+
+export default Grid
--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,79 @@
-import Image from "next/image";
+import Grid from '@/Components/Grid'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import Marquee from 'react-fast-marquee'
 
-export default function Home() {
+const page = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className='w-full  h-full'>
+      <div className='w-[95%] border-0.5 border-gray-600 relative h-full mt-32 m-auto '>
+          <div className='h-[90vh] border-b w-full relative'>
+              <Grid/>
+              <Image className='w-full h-full object-cover' width={10000 } height={10000} src={'/geng.jpg'}/>
+          </div>
+
+
+          <div className=' py-10 w-full   flex items-center justify-between text-5xl font-light pl-5 text-gray-300'>
+            <p>Featured Products</p>
+          </div>
+
+          <div className=' flex  justify-between h-[50vh] relative z-10 border-b-0.5 border-gray-600  py-5 px-5'>
+              <div className=' group cursor-pointer overflow-hidden h-full relative  aspect-square '>
+                  <div className='absolute z-10 inset-0 bg-black opacity-55'></div>
+                  <div className='absolute z-10 h-[20%] bottom-0 flex flex-col  px-2 w-full text-white '>2024 GGM1 PUMA OFFICIAL JERSEY 
+                    <Link className='uppercase text-[#aa8b00] hover:underline transition-all duration-300' href={'/'}> <p> shop now -{'>'}</p> </Link>
+                  </div>
+                <Image className='w-full group-hover:scale-110 z-0 transition-all duration-700 h-full object-cover' width={10000 } height={100} src={'/jersey.webp'}/>
+              </div>
+              <div className=' group cursor-pointer overflow-hidden h-full relative  aspect-square '>
+                  <div className='absolute z-10 inset-0 bg-black opacity-55'></div>
+                  <div className='absolute z-10 h-[20%] bottom-0 flex flex-col  px-2 w-full text-white '>2024 GGM1 PUMA OFFICIAL JERSEY 
+                    <Link className='uppercase text-[#aa8b00] hover:underline transition-all duration-300' href={'/'}> <p> shop now -{'>'}</p> </Link>
+                  </div>
+                <Image className='w-full group-hover:scale-110 z-0 transition-all duration-700 h-full object-cover' width={10000 } height={100} src={'/jersey.webp'}/>
+              </div>
+              <div className=' group cursor-pointer overflow-hidden h-full relative  aspect-square '>
+                  <div className='absolute z-10 inset-0 bg-black opacity-55'></div>
+                  <div className='absolute z-10 h-[20%] bottom-0 flex flex-col  px-2 w-full text-white '>2024 GGM1 PUMA OFFICIAL JERSEY 
+                    <Link className='uppercase text-[#aa8b00] hover:underline transition-all duration-300' href={'/'}> <p> shop now -{'>'}</p> </Link>
+                  </div>
+                <Image className='w-full group-hover:scale-110 z-0 transition-all duration-700 h-full object-cover' width={10000 } height={100} src={'/jersey.webp'}/>
+              </div>
+              <div className=' group cursor-pointer overflow-hidden h-full relative  aspect-square '>
+                  <div className='absolute z-10 inset-0 bg-black opacity-55'></div>
+                  <div className='absolute z-10 h-[20%] bottom-0 flex flex-col  px-2 w-full text-white '>2024 GGM1 PUMA OFFICIAL JERSEY 
+                    <Link className='uppercase text-[#aa8b00] hover:underline transition-all duration-300' href={'/'}> <p> shop now -{'>'}</p> </Link>
+                  </div>
+                <Image className='w-full group-hover:scale-110 z-0 transition-all duration-700 h-full object-cover' width={10000 } height={100} src={'/jersey.webp'}/>
+              </div>
+              
+          </div>
+
+
+          <div className='h-[25vh] w-full mt-10 overflow-hidden border-b-0.5 border-gray-600'>
+            <Marquee className='w-full h-[25vh] '>
+              <div className='flex w-full h-[25vh]'>
+              <Image className='w-full h-[20vh] object-contain  ' width={100} height={100} src={'/hyundai-motor-company-3.svg'}/>&nbsp;&nbsp;&nbsp;&nbsp;
+              <Image className='w-full h-[25vh]   object-cover ' width={100} height={100} src={'/puma-2 (1).svg'}/>&nbsp;&nbsp;&nbsp;&nbsp;
+              <Image className='w-full h-[20vh] object-contain   ' width={100} height={100} src={'/hyundai-motor-company-3.svg'}/>&nbsp;&nbsp;&nbsp;&nbsp;
+              <Image className='w-full h-[25vh]   object-cover ' width={100} height={100} src={'/puma-2 (1).svg'}/>&nbsp;&nbsp;&nbsp;&nbsp;
+              </div>
+              <div className='flex w-full h-[25vh]'>
+              <Image className='w-full h-[20vh] object-contain  ' width={100} height={100} src={'/hyundai-motor-company-3.svg'}/>&nbsp;&nbsp;&nbsp;&nbsp;
+              <Image className='w-full h-[25vh]   object-cover ' width={100} height={100} src={'/puma-2 (1).svg'}/>&nbsp;&nbsp;&nbsp;&nbsp;
+              <Image className='w-full h-[20vh] object-contain   ' width={100} height={100} src={'/hyundai-motor-company-3.svg'}/>&nbsp;&nbsp;&nbsp;&nbsp;
+              <Image className='w-full h-[25vh]   object-cover ' width={100} height={100} src={'/puma-2 (1).svg'}/>&nbsp;&nbsp;&nbsp;&nbsp;
+              </div>
+              
+              
+            </Marquee>
+          </div>
+
+          <div className='h-[300vh]'></div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    </div>
+  )
 }
+
+export default page
